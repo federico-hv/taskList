6dad8dd3006b4185000cc5525b117ccb6c8c70fa
federico hernández velásquez <federicohernandez.ve@gmail.com>
2016-05-25 16:34:15 -0300

    Added mongo connection

--- config/connections.js
+++ config/connections.js
@@ -28,9 +28,9 @@ module.exports.connections = {
   * Installed by default.                                                    *
   *                                                                          *
   ***************************************************************************/
-  localDiskDb: {
-    adapter: 'sails-disk'
-  },
+  // localDiskDb: {
+  //   adapter: 'sails-disk'
+  // },
 
   /***************************************************************************
   *                                                                          *
@@ -56,14 +56,14 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  // someMongodbServer: {
-  //   adapter: 'sails-mongo',
-  //   host: 'localhost',
-  //   port: 27017,
-  //   user: 'username', //optional
-  //   password: 'password', //optional
-  //   database: 'your_mongo_db_name_here' //optional
-  // },
+  someMongodbServer: {
+    adapter: 'sails-mongo',
+    host: 'dokku-mongo-approveit',
+    port: 27017,
+    user: 'approveit', //optional
+    password: '7f74f44fde6440ed7f922a2b96d5e818', //optional
+    database: 'approveit' //optional
+  },
 
   /***************************************************************************
   *                                                                          *
